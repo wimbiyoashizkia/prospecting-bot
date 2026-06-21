3735acaf6dacf6ef4d3f79fd3e6712449f4139e8
Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>
2026-06-21 12:02:01 +0700

    index: Add error handling
    Improve stability by adding global handlers
    for unhandledRejection and uncaughtException.
    
    Signed-off-by: Wimbi Yoas Hizkia <wimbiyoashizkia@yahoo.com>

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ const client = new Client({
   ]
 });
 
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
+});
+
 const ores = {
   amethyst: `**Locations & Chances**
 
